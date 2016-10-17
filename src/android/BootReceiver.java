dc6bfc23177f1c2f78c9379dfd3f95cae3b334b8
Pablo Ortiz <portizb@gmail.com>
2016-10-17 16:31:04 +0200

    Create BootReceiver.java

---
+++ src/android/BootReceiver.java
@@ -0,0 +1,19 @@
+package com.movistar.tvservices.cordova.plugin;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class BootReceiver extends BroadcastReceiver {  
+	
+	/*
+	 ************************************************************************************************
+	 * Overriden Methods 
+	 ************************************************************************************************
+	 */
+	@Override  
+	public void onReceive(Context context, Intent intent) {
+		Intent serviceIntent = new Intent(context, MiViewTVService.class);         
+		context.startService(serviceIntent);
+	} 
+} 
