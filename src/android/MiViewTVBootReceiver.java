e6d30ee269fc1bc5d7fceac6399dc884c3327105
Pablo Ortiz <portizb@gmail.com>
2016-10-17 16:43:09 +0200

    Update and rename BootReceiver.java to MiViewTVBootReceiver.java

--- src/android/MiViewTVBootReceiver.java
+++ src/android/MiViewTVBootReceiver.java
@@ -4,7 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
-public class BootReceiver extends BroadcastReceiver {  
+public class MiViewTVBootReceiver extends BroadcastReceiver {  
 	
 	/*
 	 ************************************************************************************************
