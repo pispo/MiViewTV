833f406e811de31bd80226949063d7c18f7c064a
Pablo Ortiz <portizb@gmail.com>
2016-10-17 17:17:25 +0200

    Update MiViewTVBootReceiver.java

--- src/android/MiViewTVBootReceiver.java
+++ src/android/MiViewTVBootReceiver.java
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import android.util.Log;
+
 public class MiViewTVBootReceiver extends BroadcastReceiver {  
 	
 	/*
@@ -15,5 +17,7 @@ public class MiViewTVBootReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		Intent serviceIntent = new Intent(context, MiViewTVService.class);         
 		context.startService(serviceIntent);
+
+		Log.v(LOG_TAG, "getChannels");
 	} 
 } 
