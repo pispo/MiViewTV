b9ebc2a39cfa7931a01a277e27b45451b5ad059f
Pablo Ortiz <portizb@gmail.com>
2016-10-17 17:19:18 +0200

    Update MiViewTVBootReceiver.java

--- src/android/MiViewTVBootReceiver.java
+++ src/android/MiViewTVBootReceiver.java
@@ -7,17 +7,13 @@ import android.content.Intent;
 import android.util.Log;
 
 public class MiViewTVBootReceiver extends BroadcastReceiver {  
-	
-	/*
-	 ************************************************************************************************
-	 * Overriden Methods 
-	 ************************************************************************************************
-	 */
-	@Override  
-	public void onReceive(Context context, Intent intent) {
-		Intent serviceIntent = new Intent(context, MiViewTVService.class);         
-		context.startService(serviceIntent);
+    private static final String LOG_TAG = MiViewTVBootReceiver.class.getSimpleName();	
 
-		Log.v(LOG_TAG, "getChannels");
-	} 
+    @Override  
+    public void onReceive(Context context, Intent intent) {
+	Intent serviceIntent = new Intent(context, MiViewTVService.class);         
+	context.startService(serviceIntent);
+
+	Log.v(LOG_TAG, "getChannels");
+    } 
 } 
