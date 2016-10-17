7bacd393059ba4c04a018926360338a7bec9880a
Pablo Ortiz <portizb@gmail.com>
2016-10-17 17:07:12 +0200

    Update MiViewTVPlugin.java

--- src/android/MiViewTVPlugin.java
+++ src/android/MiViewTVPlugin.java
@@ -1,24 +1,26 @@
 package com.movistar.tvservices.cordova.plugin;
 
 import org.apache.cordova.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunninServiceInfo;
+import android.app.Activity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.content.ComponentName;
 import android.content.ServiceConnection;
+
 import android.os.IBinder;
-
-import android.app.Activity;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.json.JSONException;
 
 import android.util.Log;
 
 public class MiViewTVPlugin extends CordovaPlugin {
 
-    private static final String LOG_TAG = TvServicesPlugin.class.getSimpleName();
+    private static final String LOG_TAG = MiViewTVPlugin.class.getSimpleName();
 
     public static final String ACTION_GET_CHANNELS = "getChannels";
     public static final String ACTION_GET_PROGRAM_GUIDE = "getProgramGuide";
@@ -87,8 +89,10 @@ public class MiViewTVPlugin extends CordovaPlugin {
     private boolean isServiceRunning ()
     {
         try {
-            // Return Plugin with ServiceRunning true/ false
-            ActivityManager manager = (ActivityManager)this.mContext.getSystemService(Context.ACTIVITY_SERVICE);
+            // Return Plugin with ServiceRunning true/ false            
+            Activity context = cordova.getActivity();
+            ActivityManager manager = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+            
             for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
                 if (MiViewTVService.class.getCanonicalName().equals(service.service.getClassName())) {
                     return true;
@@ -118,7 +122,6 @@ public class MiViewTVPlugin extends CordovaPlugin {
     private void startService()
     {
         Activity context = cordova.getActivity();
-
         Intent intent = new Intent(context, MiViewTVService.class);
 
         try {
