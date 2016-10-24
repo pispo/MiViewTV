85e2a6300c54145d41ac1a09916e5a7d1ebe9bfe
Pablo Ortiz <portizb@gmail.com>
2016-10-24 11:34:25 +0200

    Update Loader.java

--- src/android/MiViewTVLib/com/movistar/miviewtv/stb/Loader.java
+++ src/android/MiViewTVLib/com/movistar/miviewtv/stb/Loader.java
@@ -1,5 +1,11 @@
 package com.movistar.miviewtv.stb;
 
+import com.movistar.miviewtv.core.MovistarFile;
+
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.HashMap;
 
 public class Loader {
 }
