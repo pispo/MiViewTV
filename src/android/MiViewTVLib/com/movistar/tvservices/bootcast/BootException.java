abeb68c4bc84de27add5ce555624969d71da50cb
Pablo Ortiz <portizb@gmail.com>
2016-11-30 12:26:48 +0100

    Create BootException.java

---
+++ src/android/MiViewTVLib/com/movistar/tvservices/bootcast/BootException.java
@@ -0,0 +1,16 @@
+package com.movistar.miviewtv.bootconfig;
+
+import java.io.IOException;
+
+/**
+ * Exception thrown for all bootconfig parsing errors.
+ */
+
+public class BootException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    public BootException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+}
