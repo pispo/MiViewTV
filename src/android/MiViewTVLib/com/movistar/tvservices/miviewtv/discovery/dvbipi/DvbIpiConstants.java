ea0ae32810432f3b965808ea131ef2ea7646d1a5
Pablo Ortiz <portizb@gmail.com>
2016-11-30 12:39:56 +0100

    Create DvbIpiConstants.java

---
+++ src/android/MiViewTVLib/com/movistar/tvservices/miviewtv/discovery/dvbipi/DvbIpiConstants.java
@@ -0,0 +1,17 @@
+package com.movistar.tvservices.miviewtv.discovery.dvbipi;
+
+
+public class DvbIpiConstants {
+    public static final int SDS_SERVICE_PROVIDER_DISCOVERY = 0x01;
+    public static final int SDS_BROADCAST_DISCOVERY = 0x02;
+    public static final int SDS_COD_DISCOVERY = 0x03;
+    public static final int SDS_OTHER_SP_SERVICES = 0x04;
+    public static final int SDS_PACKAGE_DISCOVERY = 0x05;
+    public static final int SDS_BCG_DISCOVERY = 0x06;
+    public static final int SDS_REGIONALISATION_DISCOVERY = 0x07;
+    public static final int SDS_RMS_FUS = 0x08;
+    public static final int SDS_CONTENT_GUIDE_DISCOVERY = 0xF1;
+    public static final int SDS_COMMUNICATION_DISCOVERY = 0xF2;
+
+    public static final int SDS_DEFAULT_SECTION_ID = 0x00;
+}
