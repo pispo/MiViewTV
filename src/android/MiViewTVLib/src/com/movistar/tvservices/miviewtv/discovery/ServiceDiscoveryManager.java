d8c5ee3a8fd21740c4110ee8b8a0e317a38f4997
Pablo Ortiz <portizb@gmail.com>
2016-11-30 15:21:44 +0100

    Create ServiceDiscoveryManager.java

---
+++ src/android/MiViewTVLib/src/com/movistar/tvservices/miviewtv/discovery/ServiceDiscoveryManager.java
@@ -0,0 +1,31 @@
+package com.movistar.tvservices.miviewtv.discovery;
+
+import  com.movistar.tvservices.miviewtv.bootcast.profiles.*;
+
+/**
+ * This class implements the Service Provider that download all the info related to the Service Discovery & Selection
+ * and configuration of the Infocast service
+ */
+public class ServiceDiscoveryManager {
+
+    private static final String LOG_TAG = ServiceDiscoveryManager.class.getSimpleName();
+    private static final ServiceDiscoveryManager serviceDiscoveryManager = new ServiceDiscoveryManager();
+
+    protected ServiceDiscoveryManager() {
+    }
+
+    public ServiceDiscoveryManager getInstance() { return serviceDiscoveryManager; }
+
+    public void initialize()
+    {
+        PlatformProfile platformProfile = ProfileManager.getInstance().getPlatformProfile();
+
+        String address = platformProfile.getDvbEntryPoint().split(":")[0];
+        int port = Integer.parseInt(address[1]
+
+        DvbStpReader dvbStpReader = DvbStpReader.open(address, port);
+        Hashmap<int, MetadataContent> metadataContents = dvbStpReader.download (DvbIpiConstants.SDS_SERVICE_PROVIDER_DISCOVERY)
+        dvbStpReader.close();
+
+    }
+}
