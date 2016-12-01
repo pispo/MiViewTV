8f6830690bc78dcd66c11d04ea50dbb058ef2dc4
Pablo Ortiz <portizb@gmail.com>
2016-12-01 21:34:54 +0100

    Update miviewtv.js

--- www/miviewtv.js
+++ www/miviewtv.js
@@ -1,11 +1,55 @@
 /*global cordova, module*/
+
+var utils = require('cordova/utils'),
+  exec = require('cordova/exec'),
+  cordova = require('cordova');
+
+function MiViewTV (ipList) {
+  this.results = null;
+}
+
+MiViewTV.prototype.channels = function (success, err) {
+  var successCallback, errorCallback, self;
   
-module.exports = {
-    channels: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MiViewTVPlugin", "getChannels", []);
-    },
+  self = this;
   
-    programGuide: function (successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "MiViewTVPlugin", "getProgramGuide", []);
+  successCallback = function (r) {
+    self.results = r;
+    if (success && typeof success === 'function') {
+      success(r);
     }
+  };
+  
+  errorCallback = function (e) {
+    utils.alert('[ERROR] Error initializing Cordova: ' + e);
+    if (err && typeof err === 'function') {
+      err(e);
+    }
+  };
+  
+  exec(successCallback, errorCallback, "MiViewTVPlugin", "getChannels", []);
 };
+
+MiViewTV.prototype.programGuide = function (day, success, err) {
+  var successCallback, errorCallback, self;
+  
+  self = this;
+  
+  successCallback = function (r) {
+    self.results = r;
+    if (success && typeof success === 'function') {
+      success(r);
+    }
+  };
+  
+  errorCallback = function (e) {
+    utils.alert('[ERROR] Error initializing Cordova: ' + e);
+    if (err && typeof err === 'function') {
+      err(e);
+    }
+  };
+  
+  exec(successCallback, errorCallback, "MiViewTVPlugin", "getProgramGuide", [day]);
+};
+
+module.exports = MiViewTV;
