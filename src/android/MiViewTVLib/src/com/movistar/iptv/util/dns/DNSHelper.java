e5466032e38c7815cdc8ae78705acd856c429380
Pablo Ortiz <portizb@gmail.com>
2016-12-21 10:36:30 +0100

    Create DNSHelper.java

---
+++ src/android/MiViewTVLib/src/com/movistar/iptv/util/dns/DNSHelper.java
@@ -0,0 +1,49 @@
+package com.movistar.iptv.util.dns;
+
+import java.net.UnknownHostException;
+
+import org.xbill.DNS.ARecord;
+import org.xbill.DNS.Lookup;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Resolver;
+import org.xbill.DNS.SimpleResolver;
+import org.xbill.DNS.TextParseException;
+
+public class DNSHelper {
+    private static final String LOG_TAG = DNSHelper.class.getSimpleName();
+
+    private static final String DEFAULT_DNS_ADDR = "172.26.23.3";
+    
+    public static String resolve(String domain) {
+        String address = null;
+
+        try {
+
+            Lookup lookup = new Lookup(domain);
+            Resolver resolver = new SimpleResolver(DEFAULT_DNS_ADDR);
+            lookup.setResolver(resolver);
+
+            Record recs[] = lookup.run();
+            if (recs == null) {
+                return null;
+            }
+
+            for (Record rec : recs) {
+                address = (((ARecord) rec).getAddress()).getHostAddress();
+                break;
+            }
+
+        } catch (TextParseException e) {
+            address = null;
+
+        } catch (NullPointerException e) {
+            address = null;
+
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+            address = null;
+        }
+
+        return address;
+    }
+}
